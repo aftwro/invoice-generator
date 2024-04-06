5f636ec300cac018508b5d28d151a4a1b2a9f987
aftwro <trollingmaster1231@gmail.com>
2024-04-07 00:35:45 +0400

    Change to AED

--- src/components/InvoiceModal.jsx
+++ src/components/InvoiceModal.jsx
@@ -69,11 +69,11 @@ const InvoiceModal = ({
             // Add the page to the PDF.
             if (page) pdf.addPage();
 
-            const imgData = pageCanvas.toDataURL(`image/${imageType}`, 1);
+            const imgData = pageCanvas.toDataURL(`image/AED {imageType}`, 1);
             pdf.addImage(imgData, imageType, 0, 0, pdfWidth, pageHeight);
           }
           // Output / Save
-          pdf.save(`invoice-${invoiceInfo.invoiceNumber}.pdf`);
+          pdf.save(`invoice-AED {invoiceInfo.invoiceNumber}.pdf`);
         };
       })
       .catch((error) => {
@@ -149,10 +149,10 @@ const InvoiceModal = ({
                             {item.qty}
                           </td>
                           <td className="min-w-[80px] text-right">
-                            ${Number(item.price).toFixed(2)}
+                            AED {Number(item.price).toFixed(2)}
                           </td>
                           <td className="min-w-[90px] text-right">
-                            ${Number(item.price * item.qty).toFixed(2)}
+                            AED {Number(item.price * item.qty).toFixed(2)}
                           </td>
                         </tr>
                       ))}
@@ -162,20 +162,20 @@ const InvoiceModal = ({
                   <div className="mt-4 flex flex-col items-end space-y-2">
                     <div className="flex w-full justify-between border-t border-black/10 pt-2">
                       <span className="font-bold">Subtotal:</span>
-                      <span>${invoiceInfo.subtotal.toFixed(2)}</span>
+                      <span>AED {invoiceInfo.subtotal.toFixed(2)}</span>
                     </div>
                     <div className="flex w-full justify-between">
                       <span className="font-bold">Discount:</span>
-                      <span>${invoiceInfo.discountRate.toFixed(2)}</span>
+                      <span>AED {invoiceInfo.discountRate.toFixed(2)}</span>
                     </div>
                     <div className="flex w-full justify-between">
                       <span className="font-bold">Tax:</span>
-                      <span>${invoiceInfo.taxRate.toFixed(2)}</span>
+                      <span>AED {invoiceInfo.taxRate.toFixed(2)}</span>
                     </div>
                     <div className="flex w-full justify-between border-t border-black/10 py-2">
                       <span className="font-bold">Total:</span>
                       <span className="font-bold">
-                        $
+                        AED 
                         {invoiceInfo.total % 1 === 0
                           ? invoiceInfo.total
                           : invoiceInfo.total.toFixed(2)}
